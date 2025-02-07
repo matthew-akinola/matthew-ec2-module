11faa6dea9b0b4cb91e48639851a1972736724a8
matthew-akinola <akinolatolulope24@gmail.com>
2025-02-07 13:36:22 +0100

    fix ami issue

--- modules/ec2_instance/variable.tf
+++ modules/ec2_instance/variable.tf
@@ -10,19 +10,3 @@ variable "ec2_instances" {
     tags               = map(string)
   }))
 }
-
-
-#this is what your .tfvars values will look like
-
-  # ec2_instances = {
-  #   "instance1" = {
-  #     instance_type      = "t2.micro"
-  #     subnet_id          = "subnet-12345678"
-  #     security_groups    = ["sg-12345678"]
-  #     iam_instance_profile = "iam-profile-1"
-  #     user_data          = null # Optional, can be omitted
-  #     ec2_keypair_name   = "my-keypair"
-  #     tags               = {
-  #       Name = "Instance-1"
-  #     }
-  #   }}
